6e10ffb2979c9c13e3af90f8cf764449b5785152
jeremy-hansen-webdev <jeremy.j.hansen3420@gmail.com>
2026-01-26 16:17:41 -0500

    set up mySQL database, set up prisma and migrated model

---
+++ prisma/prisma.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+export const prisma = new PrismaClient({ adapter });
