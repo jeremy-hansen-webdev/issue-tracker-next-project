c453a497eef3206cf56aca948f741c6655480a82
jeremy-hansen-webdev <jeremy.j.hansen3420@gmail.com>
2026-01-27 13:31:19 -0500

    set up tailwind globals, react-simplemde, and created issues page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,35 @@
+"use client";
 
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 const Issues = () => {
-  return <div>Issues page</div>;
+  const [value, setValue] = useState("");
+  return (
+    <div className="p-5">
+      <h1 className="text-pr-1">Issue Page</h1>
+      <form className="flex mt-4" action="POST">
+        <div>
+          <div>
+            <label className="text-label" htmlFor="title">
+              Title
+            </label>
+            <input className="input" type="text" />
+          </div>
+          <div className="mt-3">
+            <label className="text-label mt-6" htmlFor="description">
+              Description
+            </label>
+            <SimpleMDE value={value} onChange={setValue} />
+          </div>
+          <button className="btn-pr-1">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Issues;
